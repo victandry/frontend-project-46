79d4d51cbdd8dfc6968280cdc3a5eaddaf2ea426
Victoria <vict.lebedeva@gmail.com>
2022-12-12 22:00:43 +0300

    modify index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,6 @@
 // import { test, expect } from '@jest/globals';
 import { fileURLToPath } from 'url';
-import { dirname, path } from 'path';
+import { path, dirname } from 'path';
 import generateDifference from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
