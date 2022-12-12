976c24035cce64db1a0c9a1c3496f573efe3b3f3
Victoria <vict.lebedeva@gmail.com>
2022-12-12 23:57:34 +0300

    modify index.test.js including .yml files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,36 +7,25 @@ import generateDifference from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+const expectedDifference = [
+  '{',
+  '  - follow: false',
+  '    host: hexlet.io',
+  '  - proxy: 123.234.53.22',
+  '  - timeout: 50',
+  '  + timeout: 20',
+  '  + verbose: true',
+  '}',
+];
+
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('generateDifference1', () => {
-  const expectedDifference1 = [
-    '{',
-    '  - follow: false',
-    '    host: hexlet.io',
-    '  - proxy: 123.234.53.22',
-    '  - timeout: 50',
-    '  + timeout: 20',
-    '  + verbose: true',
-    '}',
-  ];
-  expect(generateDifference(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(expectedDifference1.join('\n'));
+  const actualDifference = generateDifference(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  expect(actualDifference).toEqual(expectedDifference.join('\n'));
 });
 
 test('generateDifference2', () => {
-  const expectedDifference2 = [
-    '{',
-    '  - age: 27',
-    '  + age: 29',
-    '  - married: false',
-    '  + married: true',
-    '    name: Maria',
-    '  + place of birth: Kazan',
-    '  - surname: Ivanova',
-    '  + surname: Kuznetsova',
-    '  - university: SPbGU',
-    '  + university: SPbPU',
-    '}',
-  ];
-  expect(generateDifference(getFixturePath('file3.json'), getFixturePath('file4.json'))).toEqual(expectedDifference2.join('\n'));
+  const actualDifference = generateDifference(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
+  expect(actualDifference).toEqual(expectedDifference.join('\n'));
 });
