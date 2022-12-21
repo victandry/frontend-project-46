e6df10e51b4d528091cbab3e680e605132cf4b7a
Victoria <vict.lebedeva@gmail.com>
2022-12-21 23:10:04 +0300

    add action with stylish

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,7 @@
 
 import { program } from 'commander';
 import generateDifference from '../src/index.js';
+import makeStylish from '../src/stylish.js';
 
 program
   .name('gendiff')
@@ -9,5 +10,10 @@ program
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2) => console.log(generateDifference(filepath1, filepath2)))
+  .action((filepath1, filepath2, type) => {
+    if (type === 'stylish') {
+      return makeStylish(generateDifference(filepath1, filepath2));
+    }
+    return 1;
+  })
   .parse(process.argv);
