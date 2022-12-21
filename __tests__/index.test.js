52c961c71460f995cdeb977b4c33b5b22a07fc0c
Victoria <vict.lebedeva@gmail.com>
2022-12-21 22:21:52 +0300

    fixed mistakes in tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -7,7 +7,7 @@ import generateDifference from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const expectedDifference = [
+const expectedFlatDifference = [
   '{',
   '  - follow: false',
   '    host: hexlet.io',
@@ -18,14 +18,61 @@ const expectedDifference = [
   '}',
 ];
 
+const expectedDifference = [
+  '{',
+  '    common: {',
+  '      + follow: false',
+  '        setting1: Value 1',
+  '      - setting2: 200',
+  '      - setting3: true',
+  '      + setting3: null',
+  '      + setting4: blah blah',
+  '      + setting5: {',
+  '            key5: value5',
+  '        }',
+  '        setting6: {',
+  '            doge: {',
+  '              - wow: ',
+  '              + wow: so much',
+  '            }',
+  '            key: value',
+  '          + ops: vops',
+  '        }',
+  '    }',
+  '    group1: {',
+  '      - baz: bas',
+  '      + baz: bars',
+  '        foo: bar',
+  '      - nest: {',
+  '            key: value',
+  '        }',
+  '      + nest: str',
+  '    }',
+  '  - group2: {',
+  '        abc: 12345',
+  '        deep: {',
+  '            id: 45',
+  '        }',
+  '    }',
+  '  + group3: {',
+  '        deep: {',
+  '            id: {',
+  '                number: 45',
+  '            }',
+  '        }',
+  '        fee: 100500',
+  '    }',
+  '}',
+];
+
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('generateDifference1', () => {
-  const actualDifference = generateDifference(getFixturePath('file1.json'), getFixturePath('file2.json'));
+  const actualDifference = generateDifference(getFixturePath('file3.json'), getFixturePath('file4.json'));
   expect(actualDifference).toEqual(expectedDifference.join('\n'));
 });
 
 test('generateDifference2', () => {
   const actualDifference = generateDifference(getFixturePath('file1.yml'), getFixturePath('file2.yml'));
-  expect(actualDifference).toEqual(expectedDifference.join('\n'));
+  expect(actualDifference).toEqual(expectedFlatDifference.join('\n'));
 });
