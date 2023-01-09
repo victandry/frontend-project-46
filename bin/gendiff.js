d9de749aa4d40f3e40f8405c3d662c494883309d
Victoria <vict.lebedeva@gmail.com>
2023-01-09 21:07:34 +0300

    modify gendiff.js stylish as default

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import generateDifference from '../src/index.js';
+// import generateDifference from '../src/index.js';
 import makeStylish from '../src/stylish.js';
 
 program
@@ -10,10 +10,9 @@ program
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .action((filepath1, filepath2, type) => {
-    if (type === 'stylish') {
-      return makeStylish(generateDifference(filepath1, filepath2));
+  .action((filepath1, filepath2) => {
+    if (program.opts().format === 'stylish') {
+      console.log(makeStylish(filepath1, filepath2));
     }
-    return 1;
   })
   .parse(process.argv);
