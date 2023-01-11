17548f4ca076e37cca6659bc8a9bd9ae87844c04
Victoria <vict.lebedeva@gmail.com>
2023-01-12 00:38:18 +0300

    modify formatters/index.js

--- formatters/index.js
+++ formatters/index.js
@@ -1,5 +1,6 @@
 import makePlain from './plain.js';
 import makeStylish from './stylish.js';
+import makeJson from './json.js';
 
 const genDiff = (filepath1, filepath2, formatName) => {
   switch (formatName) {
@@ -7,6 +8,8 @@ const genDiff = (filepath1, filepath2, formatName) => {
       return makeStylish(filepath1, filepath2);
     case 'plain':
       return makePlain(filepath1, filepath2);
+    case 'json':
+      return makeJson(filepath1, filepath2);
     default:
       return 'no such format';
   }
