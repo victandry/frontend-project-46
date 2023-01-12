afb30543948ca6a64bc83f4f6fca05065810652a
Victoria <vict.lebedeva@gmail.com>
2023-01-12 23:49:34 +0300

    modify src/index.js

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ const isObject = (value) => (value === Object(value) && !Array.isArray(value));
 const generateDifference = (file1, file2) => {
   const file1Keys = Object.keys(file1);
   const file2Keys = Object.keys(file2);
-
   const sortedKeys = _.union(file1Keys, file2Keys).sort();
 
   const keyStates = sortedKeys
@@ -36,7 +35,6 @@ const generateDifference = (file1, file2) => {
       }
       return acc;
     }, {});
-
   return keyStates;
 };
 
