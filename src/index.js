a9d6643327272b07a578504a4c6c373281528a1b
Victoria <vict.lebedeva@gmail.com>
2023-01-12 23:38:17 +0300

    modify src/index.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ const buildAbsolutePath = (filepath) => path.resolve(process.cwd(), path.extname
 
 const parseFile = (filepath) => parse(fs.readFileSync(filepath), path.extname(filepath));
 
-const isObject = (value) => (value === Object(value) && !Array.isArray(value));
-
 const generateDifference = (file1, file2) => {
   const file1Keys = Object.keys(file1);
   const file2Keys = Object.keys(file2);
@@ -25,7 +23,7 @@ const generateDifference = (file1, file2) => {
         acc[key] = 'removed';
         return acc;
       }
-      if (isObject(file1[key]) && isObject(file2[key])) {
+      if (_.isObject(file1[key]) && _.isObject(file2[key])) {
         acc[key] = generateDifference(file1[key], file2[key]);
         return acc;
       }
