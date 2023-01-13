2941c44ea8b8abc7195e324a06190515cf010563
Victoria <vict.lebedeva@gmail.com>
2023-01-13 21:41:14 +0300

    modify stylish.js

--- formatters/stylish.js
+++ formatters/stylish.js
@@ -1,5 +1,5 @@
 import _ from 'lodash';
-import { generateDifference, parseFile, buildAbsolutePath } from '../src/index.js';
+import generateDifference, { parseFile, buildAbsolutePath } from '../src/index.js';
 
 const stringify = (data, replacer = ' ', spacesCount = 1) => {
   const iter = (obj, depth) => {
