77d1320c69bcdfcc840fd3d63d351f527c563cca
Victoria <vict.lebedeva@gmail.com>
2023-01-16 21:54:24 +0300

    modify src/parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -3,13 +3,10 @@ import yaml from 'js-yaml';
 // Выбирается функция-парсер в зависимости от расширения файла
 
 const parse = (data, extension) => {
-  let parserFunction;
   if (extension === '.json') {
-    parserFunction = JSON.parse;
-  } else if (extension === '.yml' || extension === '.yaml') {
-    parserFunction = yaml.load;
+    return JSON.parse(data);
   }
-  return parserFunction(data);
+  return yaml.load(data);
 };
 
 export default parse;
