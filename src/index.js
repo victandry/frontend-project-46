194aa98dd262022483d472d2dc4b0fefa67d5b8b
Victoria <vict.lebedeva@gmail.com>
2023-01-16 23:20:54 +0300

    modify src/index.js

--- src/index.js
+++ src/index.js
@@ -12,32 +12,24 @@ const isObject = (value) => (value === Object(value) && !Array.isArray(value));
 const generateDifference = (file1, file2) => {
   const file1Keys = Object.keys(file1);
   const file2Keys = Object.keys(file2);
-  const unitedKeys = _.union(file1Keys, file2Keys).sort();
+  const unitedKeys = _.union(file1Keys, file2Keys);
   const sortedKeys = _.cloneDeep(unitedKeys).sort();
 
   const keyStates = sortedKeys
     .reduce((acc, key) => {
-      if (!_.has(acc, key)) {
-        acc[key] = '';
-      }
       if (!_.has(file1, key)) {
-        acc[key] = 'added';
-        return acc;
+        return { ...acc, [key]: 'added' };
       }
       if (!_.has(file2, key)) {
-        acc[key] = 'removed';
-        return acc;
+        return { ...acc, [key]: 'removed' };
       }
       if (isObject(file1[key]) && isObject(file2[key])) {
-        acc[key] = generateDifference(file1[key], file2[key]);
-        return acc;
+        return { ...acc, [key]: `${generateDifference(file1[key], file2[key])}` };
       }
       if (file1[key] !== file2[key]) {
-        acc[key] = 'changed';
-      } else {
-        acc[key] = 'unchanged';
+        return { ...acc, [key]: 'changed' };
       }
-      return acc;
+      return { ...acc, [key]: 'unchanged' };
     }, {});
   return keyStates;
 };
