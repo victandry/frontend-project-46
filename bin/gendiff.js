cfed6778fa4eafd2a8f7ebb8f2f99b40edfc0457
Victoria <vict.lebedeva@gmail.com>
2023-01-20 23:21:35 +0300

    modifying the paths

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import genDiff from '../formatters/index.js';
+import genDiff from '../src/formatters/index.js';
 
 program
   .name('gendiff')
