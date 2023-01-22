7576434b9c20db3257b3402b77d5b3b18d9ce532
Victoria <vict.lebedeva@gmail.com>
2023-01-22 17:02:44 +0300

    modify index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,30 +1,47 @@
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-import * as path from 'path';
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+// import * as path from 'path';
 import * as fs from 'fs';
-import makeStylish from '../src/formatters/stylish.js';
-import makePlain from '../src/formatters/plain.js';
-import makeJson from '../src/formatters/json.js';
+import genDiff from '../src/formatters/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+// const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-test('generateStylishDifference', () => {
-  const actualDifference = makeStylish(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  const expectedStylishDifference = fs.readFileSync('__fixtures__/stylishDifference.txt').toString();
-  expect(actualDifference).toEqual(expectedStylishDifference);
-});
+describe('Generate difference (gendiff)', () => {
+  it('should compare json files', () => {
+    const actualStylishDifference = genDiff('__fixtures__/file1.json', '__fixtures__/file2', 'stylish');
+    const expectedStylishDifference = fs.readFileSync('__fixtures__/stylishDifference.txt').toString();
+    expect(actualStylishDifference).toEqual(expectedStylishDifference);
 
-test('generatePlainDifference', () => {
-  const actualDifference = makePlain(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  const expectedPlainDifference = fs.readFileSync('__fixtures__/plainDifference.txt').toString();
-  expect(actualDifference).toEqual(expectedPlainDifference);
-});
+    const actualPlainDifference = genDiff('__fixtures__/file1.json', '__fixtures__/file2', 'plain');
+    const expectedPlainDifference = fs.readFileSync('__fixtures__/plainDifference.txt').toString();
+    expect(actualPlainDifference).toEqual(expectedPlainDifference);
 
-test('generateJsonDifference', () => {
-  const actualDifference = makeJson(getFixturePath('file1.json'), getFixturePath('file2.json'));
-  const expectedJsonDifference = fs.readFileSync('__fixtures__/jsonDifference.txt').toString();
-  expect(actualDifference).toEqual(expectedJsonDifference);
+    const actualJsonDifference = genDiff('__fixtures__/file1.json', '__fixtures__/file2', 'json');
+    const expectedJsonDifference = fs.readFileSync('__fixtures__/jsonDifference.txt').toString();
+    expect(actualJsonDifference).toEqual(expectedJsonDifference);
+
+    const actualUndefinedDifference = genDiff('__fixtures__/file1.json', '__fixtures__/file2', '');
+    const expectedUndefinedDifference = fs.readFileSync('__fixtures__/stylishDifference.txt').toString();
+    expect(actualUndefinedDifference).toEqual(expectedUndefinedDifference);
+  });
+  it('should compare yml files', () => {
+    const actualStylishDifference = genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'stylish');
+    const expectedStylishDifference = fs.readFileSync('__fixtures__/stylishDifference.txt').toString();
+    expect(actualStylishDifference).toEqual(expectedStylishDifference);
+
+    const actualPlainDifference = genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'plain');
+    const expectedPlainDifference = fs.readFileSync('__fixtures__/plainDifference.txt').toString();
+    expect(actualPlainDifference).toEqual(expectedPlainDifference);
+
+    const actualJsonDifference = genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'json');
+    const expectedJsonDifference = fs.readFileSync('__fixtures__/jsonDifference.txt').toString();
+    expect(actualJsonDifference).toEqual(expectedJsonDifference);
+
+    const actualUndefinedDifference = genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', '');
+    const expectedUndefinedDifference = fs.readFileSync('__fixtures__/stylishDifference.txt').toString();
+    expect(actualUndefinedDifference).toEqual(expectedUndefinedDifference);
+  });
 });
