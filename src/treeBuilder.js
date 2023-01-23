b904400d5b86142d0fb823ca90ea9cd9df1c823d
Victoria <vict.lebedeva@gmail.com>
2023-01-23 22:23:17 +0300

    modify treeBuilder.js adding keys, types and values

--- src/treeBuilder.js
+++ src/treeBuilder.js
@@ -2,27 +2,52 @@ import _ from 'lodash';
 
 const isObject = (value) => (value === Object(value) && !Array.isArray(value));
 
-const generateDifference = (file1, file2) => {
-  const file1Keys = Object.keys(file1);
-  const file2Keys = Object.keys(file2);
-  const sortedKeys = _.sortBy(_.union(file1Keys, file2Keys));
+const generateDifference = (data1, data2) => {
+  const data1Keys = Object.keys(data1);
+  const data2Keys = Object.keys(data2);
+  const sortedKeys = _.sortBy(_.union(data1Keys, data2Keys));
 
   const keyStates = sortedKeys
     .reduce((acc, key) => {
-      if (!_.has(file1, key)) {
-        return { ...acc, [key]: 'added' };
+      if (!_.has(data1, key)) {
+        return [...acc, {
+          key: [key],
+          type: 'added',
+          value: data2[key],
+        },
+        ];
       }
-      if (!_.has(file2, key)) {
-        return { ...acc, [key]: 'removed' };
+      if (!_.has(data2, key)) {
+        return [...acc, {
+          key: [key],
+          type: 'removed',
+          value: data1[key],
+        },
+        ];
       }
-      if (isObject(file1[key]) && isObject(file2[key])) {
-        return { ...acc, [key]: generateDifference(file1[key], file2[key]) };
+      if (isObject(data1[key]) && isObject(data2[key])) {
+        return [...acc, {
+          key: [key],
+          type: 'nested',
+          value: generateDifference(data1[key], data2[key]),
+        },
+        ];
       }
-      if (file1[key] !== file2[key]) {
-        return { ...acc, [key]: 'changed' };
+      if (data1[key] !== data2[key]) {
+        return [...acc, {
+          key: [key],
+          type: 'changed',
+          value: [data1[key], data2[key]],
+        },
+        ];
       }
-      return { ...acc, [key]: 'unchanged' };
-    }, {});
+      return [...acc, {
+        key: [key],
+        type: 'unchanged',
+        value: data1[key],
+      },
+      ];
+    }, []);
   return keyStates;
 };
 
