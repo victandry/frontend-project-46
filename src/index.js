61280b2fd3c1ad1f305c81355f9415a2f0ba8c47
Victoria <vict.lebedeva@gmail.com>
2023-01-28 16:14:44 +0300

    modify src/index.js fixing path builders, removing extra parser

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-// import _ from 'lodash';
-import * as fs from 'fs';
 import * as path from 'path';
 import parse from './parsers.js';
 import generateDifference from './treeBuilder.js';
 import generateReport from './formatters/index.js';
 
-const buildAbsolutePath = (filepath) => path.resolve(process.cwd(), path.extname(filepath) !== '' ? filepath : `${filepath}.json`);
+const buildAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 
-const parseFile = (filepath) => parse(fs.readFileSync(filepath), path.extname(filepath));
-
-const genDiff = (filepath1, filepath2, outputFormat) => {
-  const data1 = parseFile(buildAbsolutePath(filepath1));
-  const data2 = parseFile(buildAbsolutePath(filepath2));
+const genDiff = (filepath1, filepath2, outputFormat = 'stylish') => {
+  const data1 = parse(buildAbsolutePath(filepath1));
+  const data2 = parse(buildAbsolutePath(filepath2));
   const differenceTree = generateDifference(data1, data2);
   return generateReport(differenceTree, outputFormat);
 };
