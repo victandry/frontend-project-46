2d4b5d8f7463faeb12cf13f423c01f0b0fb6fc27
Victoria <vict.lebedeva@gmail.com>
2023-01-28 16:39:40 +0300

    modify src/index.js renaming parseFile into readData

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import path from 'path';
 import parse from './parsers.js';
 import buildDiff from './treeBuilder.js';
@@ -5,9 +6,11 @@ import format from './formatters/index.js';
 
 const buildAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 
+const readData = (filepath) => parse(fs.readFileSync(filepath), path.extname(filepath));
+
 const genDiff = (filepath1, filepath2, outputFormat = 'stylish') => {
-  const data1 = parse(buildAbsolutePath(filepath1));
-  const data2 = parse(buildAbsolutePath(filepath2));
+  const data1 = readData(buildAbsolutePath(filepath1));
+  const data2 = readData(buildAbsolutePath(filepath2));
   const differenceTree = buildDiff(data1, data2);
   return format(differenceTree, outputFormat);
 };
