f440e86a67a3a531c5daf862635942fec1ef54d7
Victoria <vict.lebedeva@gmail.com>
2023-01-28 17:01:21 +0300

    modify src/parsers.js renaming fileFormat into dataFormat

--- src/parsers.js
+++ src/parsers.js
@@ -1,7 +1,7 @@
 import yaml from 'js-yaml';
 
-const parse = (data, fileFormat) => {
-  if (fileFormat === '.json') {
+const parse = (data, dataFormat) => {
+  if (dataFormat === '.json') {
     return JSON.parse(data);
   }
   return yaml.load(data);
