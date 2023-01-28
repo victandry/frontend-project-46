257b9ffb84095afe387750ddb577f8abb8e93ab1
Victoria <vict.lebedeva@gmail.com>
2023-01-28 16:52:06 +0300

    modify src/formatters/index.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,16 +2,16 @@ import makePlain from './plain.js';
 import makeStylish from './stylish.js';
 import makeJson from './json.js';
 
-const format = (differenceTree, outputFormat) => {
+const format = (diffTree, outputFormat) => {
   switch (outputFormat) {
     case 'stylish':
-      return makeStylish(differenceTree);
+      return makeStylish(diffTree);
     case 'plain':
-      return makePlain(differenceTree);
+      return makePlain(diffTree);
     case 'json':
-      return makeJson(differenceTree);
+      return makeJson(diffTree);
     default:
-      return makeStylish(differenceTree);
+      return makeStylish(diffTree);
   }
 };
 
