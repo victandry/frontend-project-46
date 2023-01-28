b167a603468cd132bf3928c4e75fc2503ea4666c
Victoria <vict.lebedeva@gmail.com>
2023-01-28 16:14:09 +0300

    modify index.test.js fixing path builders

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,21 +1,30 @@
 import * as fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, path } from 'path';
 import genDiff from '../src/index.js';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+
+const readFixture = (fixtureName) => fs.readFileSync(getFixturePath(fixtureName)).toString();
+
 describe('Generate difference (gendiff)', () => {
-  const stylishResult = fs.readFileSync('__fixtures__/stylishDifference.txt').toString();
-  const plainResult = fs.readFileSync('__fixtures__/plainDifference.txt').toString();
-  const jsonResult = fs.readFileSync('__fixtures__/jsonDifference.txt').toString();
+  const stylishResult = readFixture('stylishDifference.txt');
+  const plainResult = readFixture('plainDifference.txt');
+  const jsonResult = readFixture('jsonDifference.txt');
 
   it('should compare json files', () => {
-    expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2', 'stylish')).toEqual(stylishResult);
-    expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2', 'plain')).toEqual(plainResult);
-    expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2', 'json')).toEqual(jsonResult);
-    expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2', '')).toEqual(stylishResult);
+    expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish')).toEqual(stylishResult);
+    expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'plain')).toEqual(plainResult);
+    expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'json')).toEqual(jsonResult);
+    expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(stylishResult);
   });
   it('should compare yml files', () => {
-    expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'stylish')).toEqual(stylishResult);
-    expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'plain')).toEqual(plainResult);
-    expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', 'json')).toEqual(jsonResult);
-    expect(genDiff('__fixtures__/file1.yml', '__fixtures__/file2.yml', '')).toEqual(stylishResult);
+    expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'stylish')).toEqual(stylishResult);
+    expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'plain')).toEqual(plainResult);
+    expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'), 'json')).toEqual(jsonResult);
+    expect(genDiff(getFixturePath('file1.yml'), getFixturePath('file2.yml'))).toEqual(stylishResult);
   });
 });
