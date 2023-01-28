3c63b072241dece615a07b68c0c297b2c7e7b8d0
Victoria <vict.lebedeva@gmail.com>
2023-01-28 16:25:57 +0300

    modify src/index.js renaming functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import * as path from 'path';
+import path from 'path';
 import parse from './parsers.js';
-import generateDifference from './treeBuilder.js';
-import generateReport from './formatters/index.js';
+import buildDiff from './treeBuilder.js';
+import format from './formatters/index.js';
 
 const buildAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 
 const genDiff = (filepath1, filepath2, outputFormat = 'stylish') => {
   const data1 = parse(buildAbsolutePath(filepath1));
   const data2 = parse(buildAbsolutePath(filepath2));
-  const differenceTree = generateDifference(data1, data2);
-  return generateReport(differenceTree, outputFormat);
+  const differenceTree = buildDiff(data1, data2);
+  return format(differenceTree, outputFormat);
 };
 
 export default genDiff;
