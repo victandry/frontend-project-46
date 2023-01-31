76bde1e1cfe1091651d9c9eb8a0de78ea970609c
Victoria <vict.lebedeva@gmail.com>
2023-01-31 23:27:31 +0300

    modify plain.js adding switch

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -8,31 +8,32 @@ const setValueName = (value) => {
 };
 
 const makePlain = (diffTree) => {
-  const iter = (tree, defaultPropertyName) => {
-    const nodes = tree
-      .map((node) => {
-        const propertyName = defaultPropertyName;
-        const changedPropertyName = propertyName !== '' ? `${propertyName}.${node.key}` : node.key;
-        if (node.type === 'added') {
+  const iter = (tree, defaultPropertyName) => tree
+    .map((node) => {
+      const propertyName = defaultPropertyName;
+      const changedPropertyName = propertyName !== '' ? `${propertyName}.${node.key}` : node.key;
+      switch (node.type) {
+        case 'added': {
           const valueName = setValueName(node.value);
           return `Property '${changedPropertyName}' was added with value: ${valueName}`;
         }
-        if (node.type === 'removed') {
+        case 'removed': {
           return `Property '${changedPropertyName}' was removed`;
         }
-        if (node.type === 'changed') {
+        case 'changed': {
           const valueName = `From ${setValueName(node.initValue)} to ${setValueName(node.finalValue)}`;
           return `Property '${changedPropertyName}' was updated. ${valueName}`;
         }
-        if (node.type === 'unchanged') {
+        case 'unchanged': {
           return '';
         }
-        return iter(node.children, changedPropertyName);
-      })
-      .filter((line) => line !== '')
-      .join('\n');
-    return nodes;
-  };
+        default: {
+          return iter(node.children, changedPropertyName);
+        }
+      }
+    })
+    .filter((line) => line !== '')
+    .join('\n');
   return iter(diffTree, '');
 };
 
