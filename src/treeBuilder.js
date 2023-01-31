ea93083b2bf91e28749a959d9774351aa0e359b7
Victoria <vict.lebedeva@gmail.com>
2023-01-31 23:03:11 +0300

    modify src/treeBuilder replacing value to children at type nested

--- src/treeBuilder.js
+++ src/treeBuilder.js
@@ -1,54 +1,46 @@
 import _ from 'lodash';
 
-const isObject = (value) => (value === Object(value) && !Array.isArray(value));
-
 const buildDiff = (data1, data2) => {
   const data1Keys = Object.keys(data1);
   const data2Keys = Object.keys(data2);
   const sortedKeys = _.sortBy(_.union(data1Keys, data2Keys));
 
-  const keyStates = sortedKeys
-    .reduce((acc, key) => {
+  return sortedKeys
+    .map((key) => {
       if (!_.has(data1, key)) {
-        return [...acc, {
+        return {
           key,
           type: 'added',
           value: data2[key],
-        },
-        ];
+        };
       }
       if (!_.has(data2, key)) {
-        return [...acc, {
+        return {
           key,
           type: 'removed',
           value: data1[key],
-        },
-        ];
+        };
       }
-      if (isObject(data1[key]) && isObject(data2[key])) {
-        return [...acc, {
+      if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
+        return {
           key,
           type: 'nested',
-          value: buildDiff(data1[key], data2[key]),
-        },
-        ];
+          children: buildDiff(data1[key], data2[key]),
+        };
       }
       if (data1[key] !== data2[key]) {
-        return [...acc, {
+        return {
           key,
           type: 'changed',
           value: [data1[key], data2[key]],
-        },
-        ];
+        };
       }
-      return [...acc, {
+      return {
         key,
         type: 'unchanged',
         value: data1[key],
-      },
-      ];
-    }, []);
-  return keyStates;
+      };
+    });
 };
 
 export default buildDiff;
