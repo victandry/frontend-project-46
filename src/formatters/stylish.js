d7899865319225de5e5b484825c82820bb24121d
Victoria <vict.lebedeva@gmail.com>
2023-02-06 23:42:00 +0300

    modify stylish.js removing offset

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,71 +1,48 @@
 import _ from 'lodash';
 
-const stringify = (data, replacer = ' ', spacesCount = 1) => {
-  const iter = (obj, depth) => {
-    const indentSize = depth * spacesCount;
-    const currentIndent = replacer.repeat(indentSize);
-    const bracketIndent = replacer.repeat(indentSize - spacesCount);
-    const keys = Object.keys(obj);
-    const mappedKeys = keys.flatMap((key) => {
-      const keyOutput = `${currentIndent}${key}: `;
-      const value = obj[key];
-      if (_.isObject(value)) {
-        return `${keyOutput}${iter(value, depth + 1)}`;
-      }
-      return [`${keyOutput}${value}`];
-    });
-    return ['{', ...mappedKeys, `${bracketIndent}}`].join('\n');
-  };
-  return _.isObject(data) ? iter(data, 1) : `${data}`;
-};
+const indent = (depth, isFull) => (isFull ? '    '.repeat(depth) : '    '.repeat(depth).slice(2));
 
-const offset = (str, indent) => {
-  if (str.split('\n').length === 1) {
-    return str;
-  }
-  return str
-    .split('\n')
-    .map((line) => (line !== '{' ? `${indent}${line}` : line))
-    .join('\n');
+const stringify = (data, depth) => {
+  const iter = (obj, nestLevel) => {
+    const entries = Object.entries(obj);
+    const mappedKeys = entries.flatMap(([key, value]) => {
+      if (_.isObject(value)) {
+        return `${indent(nestLevel + 1, true)}${key}: ${iter(value, nestLevel + 1)}`;
+      }
+      return `${indent(nestLevel + 1, true)}${key}: ${value}`;
+    });
+    return `{\n${mappedKeys.join('\n')}\n${indent(nestLevel, true)}}`;
+  };
+  return _.isObject(data) ? iter(data, depth) : data;
 };
 
 const formatStylish = (diffTree) => {
-  const addedKeyIndent = '  + ';
-  const removedKeyIndent = '  - ';
-  const basicIndent = '    ';
-
   const iter = (tree, depth) => {
-    const currentIndent = basicIndent.repeat(depth - 1);
     const nodes = tree
       .map((node) => {
         switch (node.type) {
           case 'removed': {
-            return `${currentIndent}${removedKeyIndent}${node.key}: ${offset(stringify(node.value, basicIndent), currentIndent + basicIndent)}`;
+            return `${indent(depth, false)}- ${node.key}: ${stringify(node.value, depth)}`;
           }
           case 'added': {
-            return `${currentIndent}${addedKeyIndent}${node.key}: ${offset(stringify(node.value, basicIndent), currentIndent + basicIndent)}`;
+            return `${indent(depth, false)}+ ${node.key}: ${stringify(node.value, depth)}`;
           }
           case 'changed': {
             return [
-              `${currentIndent}${removedKeyIndent}${node.key}: ${offset(stringify(node.value1, basicIndent), currentIndent + basicIndent)}`,
-              `${currentIndent}${addedKeyIndent}${node.key}: ${offset(stringify(node.value2, basicIndent), currentIndent + basicIndent)}`,
+              `${indent(depth, false)}- ${node.key}: ${stringify(node.value1, depth)}`,
+              `${indent(depth, false)}+ ${node.key}: ${stringify(node.value2, depth)}`,
             ].join('\n');
           }
           case 'unchanged': {
-            return `${currentIndent}${basicIndent}${node.key}: ${stringify(node.value, basicIndent)}`;
+            return `${indent(depth, true)}${node.key}: ${stringify(node.value, depth)}`;
           }
           default: {
-            return `${currentIndent}${basicIndent}${node.key}: ${stringify(iter(node.children, depth + 1))}`;
+            return `${indent(depth, true)}${node.key}: ${stringify(iter(node.children, depth + 1), depth)}`;
           }
         }
       });
-    return [
-      '{',
-      ...nodes,
-      `${currentIndent}}`,
-    ].join('\n');
+    return `{\n${nodes.join('\n')}\n${indent(depth - 1, true)}}`;
   };
-
   return iter(diffTree, 1);
 };
 
