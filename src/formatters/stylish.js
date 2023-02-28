759b328b220b689c84830b113e39eda1706d44e1
Victoria <vict.lebedeva@gmail.com>
2023-02-28 21:55:18 +0300

    modify stylish.js

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -6,45 +6,37 @@ const stringify = (data, depth) => {
   if (!_.isObject(data)) {
     return String(data);
   }
-  const mappedKeys = Object.entries(data).flatMap(([key, value]) => `${indent(depth + 1, true)}${key}: ${stringify(value, depth + 1)}`);
-  return `{\n${mappedKeys.join('\n')}\n${indent(depth, true)}}`;
+  const output = Object.entries(data).flatMap(([key, value]) => `${indent(depth + 1, true)}${key}: ${stringify(value, depth + 1)}`);
+  return `{\n${output.join('\n')}\n${indent(depth, true)}}`;
 };
 
-const iter = (tree, depth) => {
-  const nodes = tree.flatMap((node) => {
-    switch (node.type) {
-      case 'removed': {
-        return `${indent(depth, false)}- ${node.key}: ${stringify(node.value, depth)}`;
-      }
-      case 'added': {
-        return `${indent(depth, false)}+ ${node.key}: ${stringify(node.value, depth)}`;
-      }
-      case 'changed': {
-        return [
-          `${indent(depth, false)}- ${node.key}: ${stringify(node.value1, depth)}`,
-          `${indent(depth, false)}+ ${node.key}: ${stringify(node.value2, depth)}`,
-        ].join('\n');
-      }
-      case 'unchanged': {
-        return `${indent(depth, true)}${node.key}: ${stringify(node.value, depth)}`;
-      }
-      case 'nested': {
-        return [
-          `${indent(depth, true)}${node.key}: {`,
-          `${(iter(node.children, depth + 1))}`,
-          `${indent(depth, true)}}`,
-        ];
-      }
-      default:
-        throw new Error(`Wrong node type: ${node.type}.`);
+const iter = (tree, depth) => tree.flatMap((node) => {
+  switch (node.type) {
+    case 'removed': {
+      return `${indent(depth, false)}- ${node.key}: ${stringify(node.value, depth)}`;
     }
-  });
-  return [...nodes];
-};
+    case 'added': {
+      return `${indent(depth, false)}+ ${node.key}: ${stringify(node.value, depth)}`;
+    }
+    case 'changed': {
+      const output1 = `${indent(depth, false)}- ${node.key}: ${stringify(node.value1, depth)}`;
+      const output2 = `${indent(depth, false)}+ ${node.key}: ${stringify(node.value2, depth)}`;
+      return `${output1}\n${output2}`;
+    }
+    case 'unchanged': {
+      return `${indent(depth, true)}${node.key}: ${stringify(node.value, depth)}`;
+    }
+    case 'nested': {
+      const output = iter(node.children, depth + 1).join('\n');
+      return `${indent(depth, true)}${node.key}: {\n${output}\n${indent(depth, true)}}`;
+    }
+    default:
+      throw new Error(`Wrong node type: ${node.type}.`);
+  }
+});
 
 const formatStylish = (diffTree) => {
-  const output = iter(diffTree, 1)
-    .map((line) => `${line.split(',').join('\n')}`);
+  const output = iter(diffTree, 1);
   return `{\n${output.join('\n')}\n}`;
 };
 
